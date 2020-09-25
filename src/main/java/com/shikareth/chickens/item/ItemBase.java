fe9a8c425d77f16a2692c4cfae0f195a8bc2e177
Shikareth <gothic344@gmail.com>
2020-09-26 00:43:11 +0200

    Added default constructor with DEFAULT_ITEM_PROPERTIES

--- src/main/java/com/shikareth/chickens/item/ItemBase.java
+++ src/main/java/com/shikareth/chickens/item/ItemBase.java
@@ -1,9 +1,15 @@
 package com.shikareth.chickens.item;
 
+import com.shikareth.chickens.ChickensItems;
+
 import net.minecraft.item.Item;
 
 public class ItemBase extends Item{
 
+	public ItemBase() {
+		super(ChickensItems.DEFAULT_ITEM_PROPERTIES);
+	}
+	
 	public ItemBase(Properties properties) {
 		super(properties);
 	}
